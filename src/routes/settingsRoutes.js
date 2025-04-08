9ab86a9e5f7280563954da8fdc3f61f7d0f2ac3f
felipepmoreno <felipepmoreno@gmail.com>
2025-04-07 23:04:54 -0300

    .

--- src/routes/settingsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Placeholder for now - can be expanded later
-router.get('/', (req, res) => {
-  res.json({ message: 'Settings API ready' });
-});
-
-module.exports = router;
